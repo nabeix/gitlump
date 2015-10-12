43171278c442bc3a555574a9270b0872c29dbc57
Yusuke Watanabe <yusukew052@gmail.com>
2015-10-12 23:32:03 +0900

    add test

--- spec/test.js
+++ spec/test.js
@@ -1,5 +1,16 @@
-describe("test", function() {
-    it("sample", function() {
-        expect(true).toBe(true);
+var ConfigManager = require('../lib/ConfigManager').default;
+
+describe("ConfigManager", function() {
+    var manager = null;
+    beforeEach(function() {
+        manager = new ConfigManager();
+    });
+    it("constructor", function() {
+        expect(manager.config).toBe(undefined);
+    });
+    it("constructor with config", function() {
+        var config = {foo: "foo"};
+        var m = new ConfigManager(config);
+        expect(m.config).toBe(config);
     });
 });
