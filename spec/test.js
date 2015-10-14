db1103a79892c9d3ac8c0c8034fe00ef6ea5a5ce
Yusuke Watanabe <yusukew052@gmail.com>
2015-10-14 22:51:30 +0900

    update test

--- spec/test.js
+++ spec/test.js
@@ -1,4 +1,7 @@
+var fs = require("fs");
+var mkdirp = require("mkdirp");
 var ConfigManager = require('../lib/ConfigManager').default;
+var errors = require("../lib/errors");
 
 describe("ConfigManager", function() {
     var manager = null;
@@ -33,7 +36,7 @@ describe("ConfigManager", function() {
         m.load(config);
         expect(m.config).toBe(config);
     });
-    it("loadRomFile", function(done) {
+    it("loadFromFile", function(done) {
         var m = new ConfigManager();
         m.loadFromFile("./spec/data/config1.json").then(function(config) {
             expect(config).toEqual({
@@ -47,12 +50,53 @@ describe("ConfigManager", function() {
             done();
         });
     });
-    it("loadRomFile - error", function(done) {
+    it("loadFromFile - error", function(done) {
         var m = new ConfigManager();
         m.loadFromFile("./spec/data/not-exists.json").then(function(config) {
             done.fail("should be catch error.");
         }).catch(function(error) {
+            expect(error instanceof errors.ReadFileError).toBe(true);
             done();
         });;
+    });
+    it("writeToFile", function(done) {
+        mkdirp("./tmp", function(error) {
+            if (error) {
+                done.fail(error);
+            }
+            var file = "./tmp/writeToFileTest.json";
+            fs.unlinkSync(file);
+            var m = new ConfigManager();
+            m.load({f: "foo", b: "bar"});
+            m.writeToFile(file).then(function() {
+                done();
+            });
+        });
+    });
+    it("writeToFile - error", function(done) {
+        var m = new ConfigManager();
+        m.load({f: "foo", b: "bar"});
+        m.writeToFile("./not-exists-directory/writeToFileTest.json").then(function() {
+            done.fail("should be catch error.");
+        }).catch(function(error) {
+            expect(error instanceof errors.WriteFileError).toBe(true);
+            done();
+        });
+    });
+    it("repositoryConfig", function() {
+        var m = new ConfigManager();
+        m.load({
+            endpoint: "https://api.github.com/",
+            type: "user",
+            name: "myname",
+            defaultProtocol: "ssh",
+            repos: [{name: "foo", protocol: "prot", directory: "foodir"},
+                    {name: "bar", directory: "bardir"}],
+            ignore: [],
+            cloned: []
+        });
+        expect(m.repositoryConfig("baz")).toBe(null);
+        expect(m.repositoryConfig("foo")).toEqual({name: "foo", protocol: "prot", directory: "foodir"});
+        expect(m.repositoryConfig("bar")).toEqual({name: "bar", directory: "bardir"});
     });
 });
