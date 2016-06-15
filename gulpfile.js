dd5ad5fa023662fd51d15de8716a7a01caf2d98a
Yusuke Watanabe <yusukew052@gmail.com>
2016-06-16 03:04:18 +0900

    update typescript

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,9 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 var jasmine = require('gulp-jasmine');
 var reporters = require('jasmine-reporters');
+var shell = require('gulp-shell');
 
-gulp.task('compile', function(cb) {
-  exec('node_modules/typescript/bin/tsc', function (err, stdout, stderr) {
-    if (stdout) console.log(stdout);
-    if (stderr) console.log(stderr);
-    cb(err);
-  });
-});
+gulp.task('compile', shell.task('tsc'));
 
 gulp.task('test', function () {
   return gulp.src('spec/*[Test].js')
